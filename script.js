e7607c6ed8217f49ff7653708a282c98303caa2f
xtove113 <160195020+xtove113@users.noreply.github.com>
2024-02-16 10:13:46 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,379 @@
+const Top_All = [
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://www.muji.com/wp-content/uploads/sites/12/2021/02/026.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+]
+
+const Top_Knit = [
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20220829/2751882/2751882_1_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+]
+
+const Top_Tshirt = [
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20221012/2853101/2853101_16866268451208_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+]
+
+const Top_Sweatshirt = [
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+]
+
+const Top_Shirt = [
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+]
+
+const Style = [
+  {
+    productImage: 'https://image.musinsa.com/images/codimap/list/2024021508430700000014418.jpg?202402151102',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+  {
+    productImage: 'https://image.msscdn.net/images/goods_img/20230922/3586427/3586427_16953508215678_500.jpg',
+    productName: '캐주얼',
+    productDetail: '은은한 워싱코디'
+  },
+]
+
+document.querySelectorAll('.detail-category button, #shop-button, #style-button').forEach(button => {
+  button.addEventListener('click', function () {
+    // 버튼의 data-id 값에 따라 상품 정보 객체 배열 선택
+    let selectedArray;
+    switch (this.dataset.id) {
+      case 'Top-All':
+        selectedArray = Top_All;
+        break;
+      case 'Top-Knit':
+        selectedArray = Top_Knit;
+        break;
+      case 'Top-Tshirt':
+        selectedArray = Top_Tshirt;
+        break;
+      case 'Top-Sweatshirt':
+        selectedArray = Top_Sweatshirt;
+        break;
+      case 'Top-Shirt':
+        selectedArray = Top_Shirt;
+        break;
+      case 'Style':
+        selectedArray = Style;
+        break;
+      default:
+        console.log('Unknown category');
+        return;
+    }
+
+    // .fashion-items 내부의 img, h2, p 태그 내용 변경
+    const fashionItems = document.querySelectorAll('.fashion-items .fashion-item');
+    fashionItems.forEach((item, index) => {
+      if (index < selectedArray.length) {
+        item.querySelector('img').src = selectedArray[index].productImage;
+        item.querySelector('h2').textContent = selectedArray[index].productName;
+        item.querySelector('p').textContent = selectedArray[index].productDetail;
+      }
+    });
+  });
+});
+
+const categoryButtons = document.querySelectorAll('.category-button');
+
+categoryButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const buttonDataId = button.getAttribute('data-id');
+
+    const detailCategory = document.querySelector(`.detail-category[data-id="${buttonDataId}"]`);
+
+    // 'detail-category'의 현재 높이값을 확인
+    const currentHeight = detailCategory.style.height;
+
+    detailCategory.style.height = currentHeight === 'auto' ? '0' : 'auto';
+    detailCategory.style.overflow = currentHeight === 'auto' ? 'hidden' : 'visible';
+
+    const icon = button.querySelector('#plus_minus_icon');
+
+    const currentSrc = icon.getAttribute('src');
+
+    icon.setAttribute('src', currentSrc === './assets/plus.svg' ? './assets/minus.svg' : './assets/plus.svg');
+  });
+})
